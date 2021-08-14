bd7b5a7fac0341e8df7f8df5921a933887aaf857
Vadym Shturkhal <vadymshturkhal@gmail.com>
2021-08-14 13:43:31 +0300

    add carcas search BFS based

--- carcas-search.js
+++ carcas-search.js
@@ -1,8 +1,9 @@
-const graph = require("./graph");
+const Graph = require("./graph");
+const Queue = require("./queue");
 
-function carcasSearchDFSBased(G, vertexFrom, checkedVertexies, T) {
+function carcasSearchDFSBased(graph, vertexFrom, checkedVertexies, T) {
   if (!checkedVertexies) {
-    checkedVertexies = G.getAllVertexies();
+    checkedVertexies = graph.getAllVertexies();
     T = new Set();
   };
 
@@ -11,10 +12,33 @@ function carcasSearchDFSBased(G, vertexFrom, checkedVertexies, T) {
   };
 
   checkedVertexies[vertexFrom - 1] = false;
-  for (let u of G.getVertexNeighbours(vertexFrom)) {
+  for (let u of graph.getVertexNeighbours(vertexFrom)) {
     if (checkedVertexies[u - 1]) {
       T.add([vertexFrom, u])
-      carcasSearchDFSBased(G, u, checkedVertexies, T);
+      carcasSearchDFSBased(graph, u, checkedVertexies, T);
+    };
+  };
+
+  return T;
+};
+
+function carcasSearchBFSBased(graph, v) {
+  const queue = new Queue();
+  const T = new Set();
+  queue.put(v);
+
+  const allVertexies = graph.getAllVertexies();
+  allVertexies[v - 1] = false;
+
+  while (queue.length) {
+    let currentVertex = queue.pick();
+
+    for (neighbour of graph.getVertexNeighbours(currentVertex)) {
+      if (allVertexies[neighbour - 1]) {
+        T.add([currentVertex, neighbour])
+        allVertexies[neighbour - 1] = false;
+        queue.put(neighbour);
+      };
     };
   };
 
@@ -39,10 +63,12 @@ function carcasSearchDFSBased(G, vertexFrom, checkedVertexies, T) {
 //   13: [6],
 // };
 
-// const G = new graph(obj);
-// const val = carcasSearchDFSBased(G, 1);
-// console.log(val);
+// const graph = new Graph(obj);
+// console.log(carcasSearchDFSBased(graph, 1));
+// console.log();
+// console.log(carcasSearchBFSBased(graph, 1));
 
 module.exports = {
   'carcasSearchDFSBased': carcasSearchDFSBased,
+  'carcasSearchBFSBased': carcasSearchBFSBased,
 };
